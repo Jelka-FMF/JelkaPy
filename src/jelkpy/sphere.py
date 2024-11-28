f5bf40f4a78e591fff5aefafe192d05a3a3bb895
Bor Brudar <bor.brudar@gmail.com>
2024-11-28 10:42:57 +0100

    barbers

--- src/jelkpy/sphere.py
+++ src/jelkpy/sphere.py
@@ -1,5 +1,11 @@
+from .types import Position
+from .util import distance
 
 class Sphere:
-    def __init__(self, radius):
+    def __init__(self, center : Position,radius : float):
+        self.center = center
         self.radius = radius
     
+
+    def is_inside(self, pt: Position) -> bool:
+        return distance(self.center, pt) <= self.radius
