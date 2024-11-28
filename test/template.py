2775f3d09ec91c6aa14d3c6dccb796ff8f819a69
Bor Brudar <bor.brudar@gmail.com>
2024-11-28 11:02:19 +0100

    pulse

--- test/template.py
+++ test/template.py
@@ -1,12 +1,13 @@
 from jelkpy.jelka import Jelka,Color
 
+def init(jelka : Jelka):
+    None
 
 def callback(jelka: Jelka):
     None
 
-
 def main():
     jelka = Jelka(300, 60)
-    jelka.run(callback)
+    jelka.run(callback,init)
 
 main()
